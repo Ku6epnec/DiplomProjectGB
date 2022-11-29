fce73cc3fb409c2c5ec97b513e833e0152b6b8c9
Ku6epnec <ku6epnec@rambler.ru>
2022-11-29 17:36:52 +0300

    Add Music UI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,14 +7,22 @@ public class Player : MonoBehaviour
     public static int _health;
     [SerializeField] private int _maxHealth = 5;
     [SerializeField] private GameObject _loseCanvas;
+    [SerializeField] private GameObject _menuCanvas;
+
+    private bool _menuState = false;
 
     private void Start()
     {
         _health = _maxHealth;
     }
 
-    void FixedUpdate()
+    void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _menuState = !_menuState;
+            _menuCanvas.SetActive(_menuState);
+        }
         if (_health < 1)
         {
             Debug.Log("PlayerDeath!!!");
